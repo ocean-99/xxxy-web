4ccd1a55ed2151dca441bd128efea7e6126547a9
zsvg <8680782@qq.com>
2022-10-09 17:32:05 +0800

    工作流优化补充

--- src/stores/themeConfig.ts
+++ src/stores/themeConfig.ts
@@ -124,8 +124,8 @@ export const useThemeConfig = defineStore('themeConfig', {
 			 * 后端控制路由
 			 */
 			// 是否开启后端控制路由
-			// isRequestRoutes: false,
-			isRequestRoutes: true,
+			isRequestRoutes: false,
+			// isRequestRoutes: true,
 
 			/**
 			 * 全局网站标题 / 副标题
